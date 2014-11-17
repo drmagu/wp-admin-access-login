478d73bf3c82cbd3a892a200c08652f780772b06
Willy Wouters <willy@drmagu.com>
2014-11-16 22:24:10 -0600

    MVC Implemented - 0.9.3

--- limit-admin-access-login.php
+++ limit-admin-access-login.php
@@ -9,13 +9,11 @@ Author URI: http://www.drmagu.com
 */
 
 require_once(__DIR__.'/includes/LimitAdminAccess.class.php');
-//require_once(__DIR__.'/includes/LoginForm.class.php');
 require_once(__DIR__.'/includes/Controller.class.php');
 require_once(__DIR__.'/includes/LoginModel.class.php');
 require_once(__DIR__.'/includes/LoginView.class.php');
 
 new LimitAdminAccess();
-//new LoginForm($_POST);
 $dbs_model = new LoginModel($_POST);
 $dbs_view = new LoginView($dbs_model);
 new Controller($_POST, $dbs_view, $dbs_model);
